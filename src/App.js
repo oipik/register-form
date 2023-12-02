18cb55461eb399b2c8097b8c27e82bfbeae2c91d
oipik <wazuup.lw@mail.ru>
2023-12-02 16:55:16 +0300

    end html css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Register from './components/Register';
+
 import './App.css';
 
 function App() {
     return (
         <div className="App">
-
+            <Register />
         </div>
     );
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,8 +1,31 @@
+import { useState } from "react";
 
+import '../../src/reset.css'
+import './register.scss';
 
 const Register = () => {
     return (
-
+        <div className="wrapper">
+            <div className="container">
+                <form className="wrapper__form">
+                    <h4 className="wrapper__title">LocStar</h4>
+                    <input 
+                        className="wrapper__input firstname" 
+                        type="text" 
+                        placeholder="Full Name"/>
+                    <input 
+                        className="wrapper__input email" 
+                        type="email" 
+                        placeholder="Email"/>
+                    <input 
+                        className="wrapper__input password" 
+                        type="password" 
+                        placeholder="Password"/>
+                    <button className="wrapper__btn">Create Account</button>
+                    <p className="wrapper__text">By clicking “Create Account” you agree to our terms and privacy policy.</p>
+                </form>
+            </div>
+        </div>
     )
 }
 
